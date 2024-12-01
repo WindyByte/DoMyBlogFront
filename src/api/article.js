9cd438fa5012322f7f5a76b175152d2b53155f0b
wanglei.75 <wanglei.75@bytedance.com>
2024-12-01 10:16:03 +0800

    添加动画，相关文章sidebar

---
+++ src/api/article.js
@@ -0,0 +1,3 @@
+export default {
+    relatedArticleList: 'http://127.0.0.1:8080/api/v1/article/related', // 相关的十篇文章title
+}
---
+++ src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/post">Blog</router-link>
+      <router-link to="/animation">Animation</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+  </header>
+</template>
+
+<style>
+header {
+  background-color: #333;
+  color: bisque;
+  padding: 1em;
+  text-align: center;
+}
+nav a {
+  color: bisque;
+  margin: 0 1em;
+  text-decoration: none;
+}
+</style>
+<script setup lang="ts">
+</script>
